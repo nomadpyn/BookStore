6c547796e0402126be671246bcc647961c665a5f
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-14 15:47:18 +0700

    Add QuitCommand

---
+++ BookStore/BookStore.InventoryManagement/Commands/QuitCommand.cs
@@ -0,0 +1,34 @@
+﻿#region Usings
+using BookStore.InventoryManagement.Commands.Abstract;
+using BookStore.InventoryManagement.UserInterface.Abstract;
+#endregion
+
+namespace BookStore.InventoryManagement.Commands
+{
+    /// <summary>
+    /// Команда выхода из приложения
+    /// </summary>
+    internal class QuitCommand : InventoryCommand
+    {
+        #region Constructors
+
+        internal QuitCommand(IUserInterface userInterface) : base(true, userInterface) { }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Переопределение тела команды
+        /// </summary>
+        /// <returns></returns>
+        internal override bool InternalCommand()
+        {
+            UserInterface.WriteMessage("Спасибо что пользовались системой управления BookStore!");
+
+            return true;
+        }
+
+        #endregion
+    }
+}
