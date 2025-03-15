be508c756eba473ee1d5d20b1e2da271f3970444
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-15 21:07:29 +0700

    Add model Book

---
+++ BookStore/BookStore.InventoryManagement/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿
+namespace BookStore.InventoryManagement.Models
+{
+    /// <summary>
+    /// Модель представления книги
+    /// </summary>
+    public class Book
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Идентификатор книги
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование книги
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Количество книг
+        /// </summary>
+        public int Quantity { get; set; }
+
+        #endregion
+    }
+}
