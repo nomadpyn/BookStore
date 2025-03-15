fa92d16be1baaeae09520fe8d4b2db739ea4e347
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-15 21:52:42 +0700

    Update InventoryContext Tests

--- BookStore/BookStore.InventoryManagement.Tests/InventoryContextTests/InventoryContextTests.cs
+++ BookStore/BookStore.InventoryManagement.Tests/InventoryContextTests/InventoryContextTests.cs
@@ -10,28 +10,69 @@ namespace BookStore.InventoryManagement.Tests.InventoryContextTests
         [TestMethod]
         public void MaintainBooks_Successful()
         {
-            var context = new InventoryContext();
+            List<Task> tasks = new();
 
             foreach (var id in Enumerable.Range(1, 30))
             {
-                context.AddBook($"Book_{id}");
+                tasks.Add(AddBook($"Book_{id}"));
             }
+
+            Task.WaitAll(tasks.ToArray());
+
+            tasks.Clear();
 
             foreach (var quantity in Enumerable.Range(1, 10))
             {
                 foreach (var id in Enumerable.Range(1, 30))
                 {
-                    context.UpdateQuantity($"Book_{id}", quantity);
+                    tasks.Add(UpdateQuantity($"Book_{id}", quantity));
                 }
             }
 
+            Task.WaitAll(tasks.ToArray());
+
+            var context = InventoryContext.Singleton;
+
             var Books = context.GetBooks();
+
             Assert.IsTrue(Books.Count == 30);
 
-            foreach(var book in Books)
+            foreach (var book in Books)
             {
                 Assert.AreEqual(55, book.Quantity);
             }
+        }
+
+
+        /// <summary>
+        /// Добавление книги из отдельного потока
+        /// </summary>
+        /// <param name="bookName"></param>
+        /// <returns></returns>
+        public Task AddBook(string bookName)
+        {
+            return Task.Run(() =>
+            {
+                var context = InventoryContext.Singleton;
+
+                Assert.IsTrue(context.AddBook(bookName));
+            });
+        }
+
+        /// <summary>
+        /// Обновление количества книг из отдельного потока
+        /// </summary>
+        /// <param name="bookName"></param>
+        /// <param name="quantity"></param>
+        /// <returns></returns>
+        public Task UpdateQuantity(string bookName, int quantity)
+        {
+            return Task.Run(() =>
+            {
+                var context = InventoryContext.Singleton;
+
+                Assert.IsTrue(context.UpdateQuantity(bookName,quantity));
+            });
         }
     }
 }
