3de6284efb7bdd03473c11aec22312ca04a5cad2
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-17 11:21:22 +0700

    Update Client

--- BookStore/BookStore.InventoryManagementClient/Program.cs
+++ BookStore/BookStore.InventoryManagementClient/Program.cs
@@ -1,2 +1,26 @@
-﻿
+﻿#region Usings
+using BookStore.InventoryManagement.CommandFactory;
+using BookStore.InventoryManagementClient.ConsoleUserInterface;
+#endregion
+
+
 Console.WriteLine("Добро пожаловать в BookStore!");
+
+var userInterface = new ConsoleUserInterface();
+var commandFactory = new InventoryCommandFactory(userInterface);
+
+var response = commandFactory.GetCommand("?").RunCommand();
+
+while (!response.shouldQuit)
+{
+    // look at this mistake with the ToLower()
+    var input = userInterface.ReadValue("> ").ToLower();
+    var command = commandFactory.GetCommand(input);
+
+    response = command.RunCommand();
+
+    if (!response.wasSuccess)
+    {
+        userInterface.WriteMessage("Enter ? to view options.");
+    }
+}
