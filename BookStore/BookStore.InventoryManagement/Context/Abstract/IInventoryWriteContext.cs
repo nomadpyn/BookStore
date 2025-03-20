9b14b4b93a005dc36778ffa4cc2bb51ed4a34ab1
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-20 21:45:36 +0700

    Divide IInventoryContext
    IInventoryReadContext & IInventoryWriteContext

---
+++ BookStore/BookStore.InventoryManagement/Context/Abstract/IInventoryWriteContext.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BookStore.InventoryManagement.Context.Abstract
+{
+    /// <summary>
+    /// Интерфейс для чтения из контекста
+    /// </summary>
+    public interface IInventoryWriteContext
+    {
+        /// <summary>
+        /// Добавление книги
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public bool AddBook(string name);
+
+        /// <summary>
+        /// Обновление количества книг
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="quantity"></param>
+        /// <returns></returns>
+        public bool UpdateQuantity(string name, int quantity);
+    }
+}
