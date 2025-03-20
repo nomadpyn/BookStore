d2c66969b63e0116ff11108f41b00612c03caceb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-20 22:12:05 +0700

    Add models

---
+++ BookStore/BooksStore.Web/Models/Category.cs
@@ -0,0 +1,41 @@
+﻿namespace BooksStore.Web.Models
+{
+    /// <summary>
+    /// Модель категории продукта
+    /// </summary>
+    public class Category
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Id категории
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Наименование категории
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Описание категории
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Список продуктов в категории
+        /// </summary>
+        public virtual IEnumerable<Product>? Products { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Category()
+        {
+            Products = [];
+        }
+
+        #endregion
+    }
+}
---
+++ BookStore/BooksStore.Web/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿namespace BooksStore.Web.Models
+{
+    /// <summary>
+    /// Модель продукта
+    /// </summary>
+    public class Product
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Id продукта
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Наименование продукта 
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Описание продукта
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Изображение продукта
+        /// </summary>
+        public string? Image { get; set; }
+
+        /// <summary>
+        /// Цена продукта
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Id категории продукта
+        /// </summary>
+        public Guid? CategoryId { get; set; }
+
+        /// <summary>
+        /// Представление категории продукта
+        /// </summary>
+        public virtual Category? Category { get; set; }
+
+        #endregion
+    }
+}
