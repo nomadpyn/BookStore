b60b8916b066f310b6808415e99c12972909eb8d
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-21 19:19:33 +0700

    Create Controllers

---
+++ BookStore/BooksStore.Web/Controllers/CategoryController.cs
@@ -0,0 +1,83 @@
+﻿using BooksStore.Web.Models;
+using BooksStore.Web.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksStore.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IInventoryRepository _repository;
+
+        public CategoryController(IInventoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.GetCategories());
+        }
+
+        public IActionResult Details(Guid Id)
+        {
+            return View(_repository.GetCategory(Id));
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([FromBody] Category category)
+        {
+
+            try
+            {
+                _repository.AddCategory(category);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Edit(Guid Id) => View(_repository.GetCategory(Id));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid Id, [FromBody] Category category)
+        {
+
+            try
+            {
+                _repository.UpdateCategory(Id, category);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Delete(Guid Id) => View(_repository.GetCategory(Id));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Guid Id, [FromBody] Category category)
+        {
+
+            try
+            {
+                _repository.DeleteCategory(Id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
