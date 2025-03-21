a1b917a2c8f02fe224b4479f75264852c0f84911
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-21 19:19:19 +0700

    Create InventoryRepository

---
+++ BookStore/BooksStore.Web/Repository/Abstract/IInventoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using BooksStore.Web.Models;
+
+namespace BooksStore.Web.Repository.Abstract
+{
+    /// <summary>
+    /// Интерфейс для работы с репозиторием
+    /// </summary>
+    public interface IInventoryRepository
+    {
+        public Product GetProduct(Guid Id);
+
+        public IEnumerable<Product> GetProducts();
+
+        public void AddProduct(Product product);
+
+        public void UpdateProduct(Guid Id, Product product);
+
+        public void DeleteProduct(Guid Id);
+
+        public Category GetCategory(Guid Id);
+
+        public IEnumerable<Category> GetCategories();
+
+        public void AddCategory(Category product);
+
+        public void UpdateCategory(Guid Id, Category product);
+
+        public void DeleteCategory(Guid Id);
+
+    }
+}
---
+++ BookStore/BooksStore.Web/Repository/InventoryRepository.cs
@@ -0,0 +1,63 @@
+﻿#region Usings
+using BooksStore.Web.Models;
+using BooksStore.Web.Repository.Abstract;
+#endregion
+
+namespace BooksStore.Web.Repository
+{
+    /// <summary>
+    /// Репозиторий по работе с инвентарем
+    /// </summary>
+    public class InventoryRepository : IInventoryRepository
+    {
+        void IInventoryRepository.AddCategory(Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IInventoryRepository.AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IInventoryRepository.DeleteCategory(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IInventoryRepository.DeleteProduct(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Category> IInventoryRepository.GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        Category IInventoryRepository.GetCategory(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Product IInventoryRepository.GetProduct(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Product> IInventoryRepository.GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IInventoryRepository.UpdateCategory(Guid Id, Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IInventoryRepository.UpdateProduct(Guid Id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
