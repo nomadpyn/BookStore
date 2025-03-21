47573082cf10bae3d18dd89411b5e09e44f8710b
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-21 19:18:58 +0700

    Create InventoryContext

---
+++ BookStore/BooksStore.Web/Context/InventoryContext.cs
@@ -0,0 +1,22 @@
+﻿using BooksStore.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksStore.Web.Context
+{
+    public class InventoryContext : DbContext
+    {
+        #region Public Properties
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public InventoryContext() { }
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options) { }
+
+        #endregion
+    }
+}
